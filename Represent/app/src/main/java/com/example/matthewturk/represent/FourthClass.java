9a37806a8b416c42b7b0d8c5b7b85c56e1822e9e
Matt <mturk@berkeley.edu>
2016-03-04 11:56:06 -0800

    committing

--- Represent/app/src/main/java/com/example/matthewturk/represent/FourthClass.java
+++ Represent/app/src/main/java/com/example/matthewturk/represent/FourthClass.java
@@ -1,16 +1,15 @@
 package com.example.matthewturk.represent;
 
 import android.app.Activity;
-import android.widget.Button;
 import android.os.Bundle;
 
 /**
- * Created by matthewturk on 3/1/16.
+ * Created by matthewturk on 3/3/16.
  */
-public class ThirdClass extends Activity {
+public class FourthClass extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.third_layout);
+        setContentView(R.layout.fourth_layout);
     }
 }
